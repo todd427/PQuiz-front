da2bbfea3c770906835bbcedc26c4cd76288570d
todd427 <todd@toddwriter.com>
2025-06-05 16:56:39 -0700

    Fix default option value

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.onload = function () {
     const div = document.createElement('div');
     div.className = 'question';
     div.innerHTML = `${i + 1}. ${q.text} <select name="q${i + 1}">
-      <option value="0">--</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
+      <option value="">--</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
     </select>`;
     container.appendChild(div);
   });
